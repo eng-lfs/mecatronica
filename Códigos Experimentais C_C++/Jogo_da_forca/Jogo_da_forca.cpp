44d8695998cc2adee5c4ef3fcdde121671a75aa3
lucasfstos95 <lucasfstos95@gmail.com>
2019-07-08 15:31:59 -0300

    Adding code: jogo_da_forca.cpp

---
+++ Códigos Experimentais C_C++/Jogo_da_forca/Jogo_da_forca.cpp
@@ -0,0 +1,29 @@
+/*///###############################################################################
+  *
+  * Code was written all in portuguese
+  * Jogo da Forca
+  * Criado por: Lucas Felipe - lucas.fstos95@gmail.com
+  * Data: 08/07/2019
+  *
+*///###############################################################################
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <new>
+
+using namespace std;
+
+void LimpaTela(){
+  system("CLS");
+}
+
+int main(){
+
+  //opção selecionada pelo usuario
+  int opcao;
+
+
+
+  return 0;
+}
